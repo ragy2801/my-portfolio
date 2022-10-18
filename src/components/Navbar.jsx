e6dcbbb17300152f7b64ac3ed902882dc8970344
ragyc <rdcostadejesus4087@eagle.fgcu.edu>
2022-10-17 23:07:05 -0400

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,80 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import { Button } from './Button'
-import './Navbar.css'
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
- function Navbar() {
-  const [click, setClick] = useState(false)
-  const [button, setButton] = useState(true)
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click)
-  const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 960) {
-      setButton(false)
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
-    else {
-      setButton(true)
-    }
-  }
+  };
 
-  window.addEventListener('resize', showButton)
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
-    <nav className='navbar'>
+      <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to='/' className='navbar-logo'>
-               Porfolio <i className='fab fa-typo3'></i> 
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-              <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            <ul className={click ? 'nam-menu active active' : 'nav-menu'}>
-              <li className="nav-item">
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                  Services
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
-                  Projects
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Portfolio
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../../App.css'
+import HeroSection from '../HeroSection.jsx'
+
+function Home(){
+    return (
+        <>
+        <HeroSection></HeroSection>
+        </>
+        
+    );
+}
+
+export default Home
